019092e2e89155410783e48528315c2829b8cac3
Linas Pelenis <linaspelenis@yahoo.com>
2021-06-09 15:21:42 +0300

    created component structure, added bg, added profile image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import './App.css';
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Resume
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          <Profile />
+        </Grid>
+        <Grid item xs>
+          <Header />
+          <Router>
+            <Switch>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/">
+                <Resume />
+              </Route>
+            </Switch>
+          </Router>
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import image from '../../assets/images/demo.jpg'
+
+import './Profile.css'
+
+const Profile = () => {
+  return (
+    <div className="profile cointainer_shadow">
+      <div className="profile_name">
+        <Typography className="name">Linas</Typography>
+        <Typography className="title">Title</Typography>
+      </div>
+
+      <figure className="profile_image">
+        <img src={image} alt="" />
+      </figure>
+
+      <div className="profile_information">
+        Insert timeline
+        <br />
+        <button>My button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
